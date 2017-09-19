5d44eeeede45d36f3f71f57019a0f56f270882c2
Brandon Antonelli <bantonelli07@gmail.com>
2017-09-19 07:54:31 -0400

    Add logo to navbar - create static directory for images - NOTE: 	- Have to reference them relative to where component code lies.

--- src/components/molecules/Navbar/Navbar.js
+++ src/components/molecules/Navbar/Navbar.js
@@ -5,8 +5,8 @@ let navbarTemplate = `
             <img 
                 class="navbar__brand-image" 
                 sizes="(max-width: 479px) 30vw, 100px" 
-                src="/images/ProEdify-Logo_FINAL_WHITE.png" 
-                srcset="/images/ProEdify-Logo_FINAL_WHITE.png 500w, /images/ProEdify-Logo_FINAL_WHITE.png 800w, /images/ProEdify-Logo_FINAL_WHITE.png 1080w, /images/ProEdify-Logo_FINAL_WHITE.png 1600w, /images/ProEdify-Logo_FINAL_WHITE.png 2000w, /images/ProEdify-Logo_FINAL_WHITE.png 2438w" 
+                src="../../../../static/images/ProEdify-Logo_FINAL_WHITE.png" 
+                srcset="../../../../static/images/ProEdify-Logo_FINAL_WHITE.png 500w, ../../..images/ProEdify-Logo_FINAL_WHITE.png 800w, ../../../../static/images/ProEdify-Logo_FINAL_WHITE.png 1080w, ../../../assets/images/ProEdify-Logo_FINAL_WHITE.png 1600w, ../../../assets/images/ProEdify-Logo_FINAL_WHITE.png 2000w, ../../../../static/images/ProEdify-Logo_FINAL_WHITE.png 2438w" 
                 width="1219"
             >
         </a>
