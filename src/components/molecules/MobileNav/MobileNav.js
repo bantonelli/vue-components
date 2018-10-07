2147f5fecc2b9440f90486e75a8213cd6070b56b
Brandon Antonelli <bantonelli07@gmail.com>
2018-10-06 22:57:30 -0400

    Update Mobile nav to render router-links if $router is available.

--- src/components/molecules/MobileNav/MobileNav.js
+++ src/components/molecules/MobileNav/MobileNav.js
@@ -1,19 +1,37 @@
 let mobileNavTemplate = `
 <div id="mobile-nav" class="mobile-nav" :class="{'is-open': isOpen}"> 
-    <div class="mobile-nav__button" @click="openMenu"> 
-        <span :class="[isOpen ? closeIcon: menuIcon]"></span>
-    </div>
-    <div class="mobile-nav__bar" :class="{'is-open': isOpen}">
-        <div class="mobile-nav__menu">
-            <a class="mobile-nav__menu-heading" href="">Really Long Username</a>
-            <ul class="mobile-nav__menu-list"> 
-                <li class="mobile-nav__menu-item" v-for="link in links" :key="link.text">
-                    <a :href="link.url" class="mobile-nav__menu-link">{{ link.text }}</a>
-                </li>                
-            </ul>
+    <template v-if="$router">
+        <div class="mobile-nav__button" @click="openMenu"> 
+            <span :class="[isOpen ? closeIcon: menuIcon]"></span>
         </div>
-    </div>
-    <slot></slot>
+        <div class="mobile-nav__bar" :class="{'is-open': isOpen}">
+            <div class="mobile-nav__menu">
+                <a class="mobile-nav__menu-heading" href="">Really Long Username</a>
+                <ul class="mobile-nav__menu-list"> 
+                    <li class="mobile-nav__menu-item" v-for="link in links" :key="link.text">
+                        <router-link :to="link" class="mobile-nav__menu-link">{{ link.name }}</a>
+                    </li>                
+                </ul>
+            </div>
+        </div>
+        <slot></slot>
+    </template>
+    <template v-else>
+        <div class="mobile-nav__button" @click="openMenu"> 
+        <span :class="[isOpen ? closeIcon: menuIcon]"></span>
+        </div>
+        <div class="mobile-nav__bar" :class="{'is-open': isOpen}">
+            <div class="mobile-nav__menu">
+                <a class="mobile-nav__menu-heading" href="">Really Long Username</a>
+                <ul class="mobile-nav__menu-list"> 
+                    <li class="mobile-nav__menu-item" v-for="link in links" :key="link.text">
+                        <a :href="link.url" class="mobile-nav__menu-link">{{ link.text }}</a>
+                    </li>                
+                </ul>
+            </div>
+        </div>
+        <slot></slot>
+    </template>
 </div>
 `;
 
@@ -42,7 +60,7 @@ export default {
                     {text: "Log Out", url: "#"}
                 ];
            }
-       } 
+       }
     },
     data: function () {
         return {
