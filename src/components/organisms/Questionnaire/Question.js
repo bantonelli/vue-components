c35ab6773ebc32e7291c3d0c46d79e64a30dad2a
Chris <chris@Chriss-Air.fios-router.home>
2018-11-03 22:59:14 -0400

    Updated Questionnaire and Question components

--- src/components/organisms/Questionnaire/Question.js
+++ src/components/organisms/Questionnaire/Question.js
@@ -1,121 +1,222 @@
+/*
+  let question know if it was answered correctly DONE
+  let parent know if correct or wrong DONE
+  PARENT PROPS: DONE
+    show answer
+    show explanations
+    show if right or wrong
+    show poll
+    submittable
+    is Poll
+  elements: DONE
+    choices
+    choice
+    .isWrong
+    .isCorrect
+    .isPoll
+    text
+    image
+    answer
+    explanation-text
+    explanation-image
+
+  CAPTURING INPUT: DONE
+    v-model on radiogroup when the question is answered will emit input event up to questionnaire
+    will also emit answerChosen event and payload object {}
+      --> this.isCorrect, this.answer (v-model from radiogroup), and this.question.question
+*/
+
 let questionTemplate = `
-  <div class="question">
-    <div id="question-form" style="width:100%;">
-      <form-component ref="form">
-        <form-item prop="choices">
-          <h6 class="typograpy-h6">{{question.question}}</h6>
-          <img :src="question.image">
-          <radio-group>
-            <radio-component v-for="choice in question.choices" :key="choice.option" :label="choice.text"></radio-component>
-          </radio-group>
-        </form-item>
-      </form-component>
-    </div>
-  </div>`;
+<div class="question">
+    <h6 class="question__text">{{question.question}}</h6>
+    <template v-if="questionImage">
+        <img :src="question.image" class="question__image">
+    </template>
+    <radio-group class="question__choices" v-model="answer">
+      <radio-component
+        :id="choice.text.replace(/\s/g, '')"
+        class="question__choice"
+        :class="{'is-correct':choiceIsCorrect(choice.text),
+        'is-wrong': choiceIsWrong(choice.text)}"
+        v-for="choice in question.choices"
+        :key="choice.text"
+        :label="choice.text">
+      </radio-component>
+    </radio-group>
 
+    <template v-if="parentProps.showAnswer">
+        <h6 class="question__answer">{{question.answer}}</h6>
+    </template>
 
-import Form from "../../organisms/Form/Form";
-import FormItem from "../../organisms/Form/FormItem";
-import RadioGroup from "../../molecules/RadioGroup/RadioGroup";
-import Radio from '../../atoms/Radio/Radio';
+    <template v-if="parentProps.showExplanation">
+        <template v-if="explanationImage">
+            <img :src="explanation.image" class="question__explanation-image">
+        </template>
+        <h6 class="question__explanation-text">{{question.explanation.explanation}}</h6>
+    </template>
+
+    <template v-if="parentProps.isPoll">
+      <template v-if="parentProps.showPoll">
+        <h6 v-for="choice in question.choices">{{choice.timesChosen}}</h6>
+      </template>
+    </template>
+
+    <template v-if="parentProps.submittable">
+      <button class="button button_color-primary" @click="chooseAnswer"><span class="button__text">Submit</span></button>
+    </template>
+
+</div>`
+
+import Form from '../../organisms/Form/Form'
+import FormItem from '../../organisms/Form/FormItem'
+import RadioGroup from '../../molecules/RadioGroup/RadioGroup'
+import Radio from '../../atoms/Radio/Radio'
+import Emitter from '../../mixins/emitter'
 
 export default {
-  template: questionTemplate,
-  data: function() {
-    return {};
-  },
-  props: {
-    question: {
-        type: Object,
-        default() {
-            return {
-
+    template: questionTemplate,
+    name: 'Question',
+    componentName: 'Question',
+    data: function () {
+        return {
+            answer: "",
+            answered: false
+        }
+    },
+    props: {
+        parentProps: {
+          type: Object,
+          default: null
+        },
+        question: {
+            type: Object,
+            default() {
+                return {
+                }
             }
         }
-    }
-  },
-  methods: {},
-  computed: {},
-  components: {
-    "radio-group": RadioGroup,
-    "form-component": Form,
-    "form-item": FormItem,
-    "radio-component": Radio
-  }
-};
+    },
+    methods: {
+      chooseAnswer() {
+        //send the event with payload to the parent
+    //     v-model on radiogroup when the question is answered will emit input event up to questionnaire
+    // will also emit answerChosen event and payload object {}
+    //   --> this.isCorrect, this.answer (v-model from radiogroup), and this.question.question
+        this.answered = true;
+        this.$emit('answerChosen', {
+          answer: this.answer,
+          question: this.question,
+          isCorrect: this.isCorrect
+        });
+      },
+      choiceIsCorrect(choiceText) {
+        if (this.answered) {
+          return this.answer === choiceText;
+        }
+      },
+      choiceIsWrong(choiceText) {
+        if (this.answered) {
+          return this.answer != choiceText;
+        }
+      }
+    },
+    mounted() {
+      this.$on('questionnaireSubmit', this.chooseAnswer);
+    },
+    computed: {
+      isCorrect() {
+        return this.question.answer === this.answer;
+      },
+      questionImage() {
+        return this.question.image != ""
+      },
+      explanationImage() {
+        return this.question.explanation.image != ""
+      }
+    },
+    components: {
+        'radio-group': RadioGroup,
+        'form-component': Form,
+        'form-item': FormItem,
+        'radio-component': Radio
+    },
+    mixins: [Emitter]
+}
 
-{/* <style lang="scss">
-#question-form {
+{
+    /* <style lang="scss">
+  #question-form {
   position: relative;
   padding: 0 50px;
   .button {
-    position: absolute;
+  position: absolute;
   }
   .checkbox-group {
-    .checkbox {
-      display: inline-block;
-      width: 45%;
-    }
+  .checkbox {
+    display: inline-block;
+    width: 45%;
+  }
   }
   .radio-group {
-    .radio {
-      margin: 10px 0 0 0;
-      display: inline-block;
-      width: 45%;
-    }
+  .radio {
+    margin: 10px 0 0 0;
+    display: inline-block;
+    width: 45%;
+  }
   }
   .select {
-    max-width: 100%;
+  max-width: 100%;
   }
   .form-item {
-    position: relative;
+  position: relative;
 
-    .form-item__label {
-      z-index: 2;
-      background: white;
-      padding: 12px 6px 12px 6px;
-      // color: #BC61A8; // nth($pe-primary-color-list, 3)
-      color: #505050;
-      font-family: "cabinbold";
-    }
+  .form-item__label {
+    z-index: 2;
+    background: white;
+    padding: 12px 6px 12px 6px;
+    // color: #BC61A8; // nth($pe-primary-color-list, 3)
+    color: #505050;
+    font-family: "cabinbold";
+  }
   }
 
   .form {
-    padding: 25px 0px;
+  padding: 25px 0px;
+  .form-item {
+    margin: 25px 0px;
+  }
+  &_label-left {
     .form-item {
-      margin: 25px 0px;
+    padding: 20px 10px;
+    margin: 10px 0;
     }
-    &_label-left {
-      .form-item {
-        padding: 20px 10px;
-        margin: 10px 0;
-      }
-      .form-item__label {
-        left: 0;
-        text-align: left;
-      }
-    }
-    &_label-right {
-      .form-item {
-        padding: 20px 10px;
-        margin: 10px 0;
-      }
-      .form-item__label {
-        left: 0;
-        text-align: right;
-      }
-    }
-    &_label-top {
-      .form-item {
-        padding: 20px 10px;
-        margin: 30px 0;
-        border-top: 1px solid transparentize(#b6b6b6, 0.5); // nth($pe-greyscale-color-list, 4);
-        border-radius: 0px;
-      }
-      .form-item__label {
-        top: -25px;
-      }
+    .form-item__label {
+    left: 0;
+    text-align: left;
     }
   }
+  &_label-right {
+    .form-item {
+    padding: 20px 10px;
+    margin: 10px 0;
+    }
+    .form-item__label {
+    left: 0;
+    text-align: right;
+    }
+  }
+  &_label-top {
+    .form-item {
+    padding: 20px 10px;
+    margin: 30px 0;
+    border-top: 1px solid transparentize(#b6b6b6, 0.5); // nth($pe-greyscale-color-list, 4);
+    border-radius: 0px;
+    }
+    .form-item__label {
+    top: -25px;
+    }
+  }
+  }
+  }
+  </style> */
 }
-</style> */}
--- src/components/organisms/Questionnaire/Questionnaire.js
+++ src/components/organisms/Questionnaire/Questionnaire.js
@@ -1,9 +1,13 @@
 let questionnaireTemplate = `<div class="questionnaire">
-  <question
-    v-for="question in shownQuestions"
-    :question="question"
-    >
-  </question>
+  <template v-for="question in shownQuestions">
+    <question
+      :question="question"
+      :parentProps="parentProps"
+      :class="modifierStyles"
+      :key=question._id
+      >
+    </question>
+  </template>
   <pagination
     @size-change="handleSizeChange"
     @current-change="handleCurrentChange"
@@ -14,27 +18,44 @@ let questionnaireTemplate = `<div class="questionnaire">
     :jumper-controls="false"
   >
   </pagination>
+  <template v-if="!submittable && lastQuestion">
+    <button class="button button_color-primary" @click="submit"><span class="button__text">Submit</span></button>
+  </template>
 </div>`;
 
 import Question from "./Question";
 import Pagination from "../Pagination/Pagination";
 import Pager from "../Pagination/Pager";
+import Emitter from "../../mixins/emitter"
 import _ from 'lodash';
 
 export default {
   template: questionnaireTemplate,
-  data: function() {
+  name: 'Questionnaire',
+  componentName: 'Questionnaire',
+  mixins: [Emitter],
+  data: function () {
     return {
       currentPage: 1,
-      pageSize: 1
+      pageSize: 1,
+      answers: []
     };
+  },
+  mounted() {
+    this.$on('answerChosen', this.handleChosenAnswer);
   },
   computed: {
     shownQuestions() {
       var amountPerPage = this.pageSize;
-      var startIndex = amountPerPage * (this.currentPage-1);
+      var startIndex = amountPerPage * (this.currentPage - 1);
       var endIndex = amountPerPage * (this.currentPage);
       return _.slice(this.questions, startIndex, endIndex);
+    },
+    lastQuestion() {
+      return this.currentPage === (this.questions.length / this.pageSize)
+    },
+    parentProps() {
+      return _.omit(this.$props, ['modifierStyles']);
     }
   },
   components: {
@@ -43,53 +64,88 @@ export default {
     pager: Pager
   },
   props: {
+    showAnswer: false,
+    showExplanation: false,
+    showRightOrWrong: false,
+    isPoll: false,
+    showPoll: false,
+    submittable: false,
+    modifierStyles: {
+      type: Array,
+      default: null
+    },
     questions: {
       type: Array,
       default: function () {
         return [
           {
+            _id: "1",
             question: "QUESTION?",
-            image: "",
+            image: "QUESTION IMAGE",
             choices: [
-              { option: "a", text: "choice a" },
-              { option: "b", text: "choice b" },
-              { option: "c", text: "choice c" },
-              { option: "d", text: "choice d" }
+              { text: "choice a" },
+              { text: "choice b" },
+              { text: "choice c" },
+              { text: "choice d" }
             ],
-            answer: ""
+            answer: "choice a",
+            explanation: {
+              answer: "choice a",
+              image: "ANSWER IMAGE",
+              explanation: "EXPLANATION"
+            }
           },
           {
+            _id: "2",
             question: "QUESTION 2?",
-            image: "",
+            image: "QUESTION IMAGE",
             choices: [
-              { option: "a", text: "choice a" },
-              { option: "b", text: "choice b" },
-              { option: "c", text: "choice c" },
-              { option: "d", text: "choice d" }
+              { text: "choice a" },
+              { text: "choice b" },
+              { text: "choice c" },
+              { text: "choice d" }
             ],
-            answer: ""
+            answer: "choice a",
+            explanation: {
+              answer: "choice a",
+              image: "ANSWER IMAGE",
+              explanation: "EXPLANATION"
+            }
           },
           {
+            _id: "3",
             question: "QUESTION 3?",
-            image: "",
+            image: "QUESTION IMAGE",
             choices: [
-              { option: "a", text: "choice a" },
-              { option: "b", text: "choice b" },
-              { option: "c", text: "choice c" },
-              { option: "d", text: "choice d" }
+              { text: "choice a", timesChosen: 0 },
+              { text: "choice b", timesChosen: 0 },
+              { text: "choice c", timesChosen: 0 },
+              { text: "choice d", timesChosen: 0 }
             ],
-            answer: ""
+            answer: "choice a",
+            explanation: {
+              answer: "choice a",
+              image: "ANSWER IMAGE",
+              explanation: "EXPLANATION"
+            }
           }
         ]
       }
     }
   },
   methods: {
-    handleSizeChange: function(value) {
+    handleSizeChange: function (value) {
       this.pageSize = value;
     },
-    handleCurrentChange: function(value) {
+    handleCurrentChange: function (value) {
       this.currentPage = value
+    },
+    submit() {
+      this.broadcast("Question", "questionnaireSubmit", []);
+      //TODO: send this.answers to backend
+    },
+    handleChosenAnswer(response) {
+      this.answers.push(response)
     }
   }
 };
